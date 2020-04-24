cfd2ab16f29f9f52014be5afdd0a968eedabaee7
Nikolay Tolev <nikolay.tolev@scalefocus.com>
2020-04-24 19:20:45 +0300

    Added Pets and contact info to owners on startup. Closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public class Pet extends BaseEntity {
 
+    private String name;
     private PetType petType;
     private Owner owner;
     private LocalDate birthDay;
@@ -30,5 +31,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDay(LocalDate birthDay) {
         this.birthDay = birthDay;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
